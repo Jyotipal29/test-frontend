f5cfb0aa1217fc9ec57739477a4243c174280fb2
jyoti pal <paljyoti0129@gmail.com>
2024-10-12 20:23:03 +0530

    added tailwind and env

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  console.log(import.meta.env.VITE_NODE_ENV);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="max-w-[400px] m-auto   py-10  flex   justify-between items-center">
+        <button
+          className="text-2xl font-bold"
+          onClick={() => setCount(count + 1)}
+        >
+          +
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p className="text-2xl font-bold">{count}</p>
+        <button
+          className="text-2xl font-bold"
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
